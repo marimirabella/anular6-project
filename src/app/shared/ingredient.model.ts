a3d9d422383c63a9b225077812ae24b7bae9edf8
Marina <marina4ya@gmail.com>
2018-08-28 22:27:20 +0300

    created model

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,11 @@
+export class Ingredient {
+  // public name: string;
+  // public amount: number;
+
+  constructor(public name: string, public amount: number) {
+    // this.name = name;
+    // this.amount = amount;
+  }
+}
+
+// can do it with less code and do the same
